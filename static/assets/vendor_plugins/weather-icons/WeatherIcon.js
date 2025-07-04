e87af663663214564e354c52f79594a27989aacc
anointing1234 <yakubudestiny9@gmail.com>
2025-07-04 17:38:45 +0100

    commted

--- static/assets/vendor_plugins/weather-icons/WeatherIcon.js
+++ static/assets/vendor_plugins/weather-icons/WeatherIcon.js
@@ -7,8 +7,8 @@
 // 
 // You are free to:
 // 
-// Share Ñ copy and redistribute the material in any medium or format
-// Adapt Ñ remix, transform, and build upon the material for any purpose, even commercially.
+// Share ï¿½ copy and redistribute the material in any medium or format
+// Adapt ï¿½ remix, transform, and build upon the material for any purpose, even commercially.
 // 
 // The licensor cannot revoke these freedoms as long as you follow the license terms.
 // 
@@ -16,10 +16,10 @@
 // 
 // Under the following terms:
 // 
-// Attribution Ñ You must give appropriate credit, provide a link to the license, and indicate if changes were made. You may do so in any reasonable manner, but not in any way that suggests the licensor endorses you or your use.
-// ShareAlike Ñ If you remix, transform, or build upon the material, you must distribute your contributions under the same license as the original.
+// Attribution ï¿½ You must give appropriate credit, provide a link to the license, and indicate if changes were made. You may do so in any reasonable manner, but not in any way that suggests the licensor endorses you or your use.
+// ShareAlike ï¿½ If you remix, transform, or build upon the material, you must distribute your contributions under the same license as the original.
 // 
-// No additional restrictions Ñ You may not apply legal terms or technological measures that legally restrict others from doing anything the license permits.
+// No additional restrictions ï¿½ You may not apply legal terms or technological measures that legally restrict others from doing anything the license permits.
 // 
 // 
 // 
@@ -28,7 +28,7 @@
 // You do not have to comply with the license for elements of the material in the public domain or where your use is permitted by an applicable exception or limitation.
 // No warranties are given. The license may not give you all of the permissions necessary for your intended use. For example, other rights such as publicity, privacy, or moral rights may limit how you use the material.
 
-"use strict";
+
 
 var WeatherIcon = (function(){
 
